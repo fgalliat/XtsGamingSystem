319db5c5a1b6568d263086c5cb63cd83a61e1721
Franck GALLIAT <fgalliat@gmail.com>
2018-12-06 20:33:39 +0100

    CLEANED UP Arduino code

--- dev/arduino/main/main.ino
+++ dev/arduino/main/main.ino
@@ -1,43 +1,45 @@
 /*************************************************************
-digitalRead.ino
+ * Xtase - fgalliat @Dec 2018
+ * 
+ * GPIO expander Manager
+ * Because of wiring SX on AriettaG25 causes too much kernel errors
+ * 
+ * needs :
+ * switch ON/OFF led {7}
+ * read GameGear pad buttons (up,left,right,down, 1,2,Start) {0..6}
+ * read DFPlayer "playing" line {8}
+ * read new add. buttons [Start bis] & [Select] {9,10}
+ * 5 lines free
+ * 
+ * TODOs :
+ * output message coding
+ * input for led (...)
+ * may be DFPlayer driving .... (I/O cmds)
+ * 
+ * based on :
 SparkFun SX1509 I/O Expander Example: digital in (digitalRead)
 Jim Lindblom @ SparkFun Electronics
 Original Creation Date: September 21, 2015
 https://github.com/sparkfun/SparkFun_SX1509_Arduino_Library
 
-This example demonstrates the SX1509's digitalRead
-functionality. A pin can either be set as an INPUT or
-INPUT_PULLUP. We'll attach an active-low button to an 
-INPUT_PULLUP input, then whenever the button read's LOW, we'll
-read the state of another INPUT pin.
-
-After uploading the sketch, open your serial monitor and set 
-it to 9600 baud.
-
 Hardware Hookup:
-	SX1509 Breakout ------ Arduino -------- Breadboard
+	SX1509 Breakout ------ Arduino
 	      GND -------------- GND
 	      3V3 -------------- 3.3V
 		  SDA ------------ SDA (A4)
 		  SCL ------------ SCL (A5)
-		  0 ---------------------------------]BTN[----GND
-		  8 -----------------------------Jumper (GND or 3.3V)
 
 Development environment specifics:
-	IDE: Arduino 1.6.5
-	Hardware Platform: Arduino Uno
+	IDE: Arduino 1.8.7
+	Hardware Platform: Arduino ProMini 328P 3.3v 8MHz
 	SX1509 Breakout Version: v2.0
-
-This code is beerware; if you see me (or any other SparkFun 
-employee) at the local, and you've found our code helpful, 
-please buy us a round!
 
 Distributed as-is; no warranty is given.
 *************************************************************/
 
 #define BUILTIN_LED 13
 
-#define SX_LED0 7
+// SX1509 Pins:
 #define SX_BTN0 0
 #define SX_BTN1 1
 #define SX_BTN2 2
@@ -45,6 +47,23 @@ Distributed as-is; no warranty is given.
 #define SX_BTN4 4
 #define SX_BTN5 5
 #define SX_BTN6 6
+#define SX_LED0 7
+#define SX_MP30 8
+#define SX_BTNA 9
+#define SX_BTNB 10
+
+// SX_BTNx are pull_up so these are pressed when '0'
+// SX_LED1 is an output so seen as '0' (or inv. logic ?)
+// [BANK A][BANK B]
+// 1111101000000000 => here means Bt [2] pressed
+
+// BT0 UP
+// BT1 RIGHT
+// BT2 LEFT
+// BT3 DOWN
+// BT4 START
+// BT5 [2] ~~ not working very well / see soldering...
+// BT6 [1]
 
 #include <Wire.h>           // Include the I2C library (required)
 #include <SparkFunSX1509.h> // Include SX1509 library
@@ -53,23 +72,15 @@ Distributed as-is; no warranty is given.
 const byte SX1509_ADDRESS = 0x3E; // SX1509 I2C address
 SX1509 io;                        // Create an SX1509 object to be used throughout
 
-// // SX1509 Pins:
-// const byte SX1509_BUTTON_PIN = 0; // Active-low button
-// const byte SX1509_INPUT_PIN = 8; // Floating or jumpered input
-
 void setup()
 {
     pinMode(BUILTIN_LED, OUTPUT);
     digitalWrite(BUILTIN_LED, LOW);
 
-    // Serial is used in this example to display the input value
-    // of the SX1509_INPUT_PIN input:
     Serial.begin(9600);
 
-Serial.println("begin...");
+    Serial.println("begin...");
 
-    // Call io.begin(<address>) to initialize the SX1509. If it
-    // successfully communicates, it'll return 1.
     if (!io.begin(SX1509_ADDRESS))
     {
         digitalWrite(BUILTIN_LED, HIGH);
@@ -78,14 +89,7 @@ Serial.println("begin...");
             ;
     }
 
-    //   // use io.pinMode(<pin>, <mode>) to set input pins as either
-    //   // INPUT or INPUT_PULLUP. Set up a floating (or jumpered to
-    //   // either GND or 3.3V) pin to an INPUT:
-    //   io.pinMode(SX1509_INPUT_PIN, INPUT);
-    //   // Use a pull-up resistor on the button's input pin. When
-    //   // the button is pressed, the pin will be read as LOW:
-    //   io.pinMode(SX1509_BUTTON_PIN, INPUT_PULLUP);
-Serial.println("Able to communicate.");
+    Serial.println("Able to communicate.");
 
     io.pinMode(SX_LED0, OUTPUT);
     io.digitalWrite(SX_LED0, HIGH);
@@ -99,27 +103,38 @@ Serial.println("Able to communicate.");
     io.pinMode(SX_BTN6, INPUT_PULLUP);
 }
 
+bool states[16];
+
 void loop()
 {
-    // use io.digitalRead(<pin>) to check if an SX1509 input
-    // pin is either HIGH or LOW.
-    //   if (io.digitalRead(SX1509_BUTTON_PIN) == LOW)
-    if (io.digitalRead(SX_BTN0) == LOW)
-    {
-        io.digitalWrite(SX_LED0, LOW);
 
-        // If the button is pressed (the pin reads LOW)
-        // Print the status of the other pin:
-        // Serial.print("SX1509_INPUT_PIN status: ");
-        Serial.print("SX_BTN1 status: ");
+    // static bool states[16];
+    // @ least one state has changed
+    bool atLeastOne = false;
+    bool curOne;
 
-        // Read the pin to print either 0 or 1
-        // Serial.println(io.digitalRead(SX1509_INPUT_PIN));
-        Serial.println(io.digitalRead(SX_BTN1));
-    }
-    else
+    for (int i = 0; i < 16; i++)
     {
-        io.digitalWrite(SX_LED0, HIGH);
+        curOne = io.digitalRead(i) == HIGH; // read pin per pin instead full BANK !
+        if (states[i] != curOne)
+        {
+            // do not store if its an OUTPUT
+            if (i != SX_LED0)
+            {
+                atLeastOne = true;
+            }
+        }
+        states[i] = curOne;
     }
+
+    if (atLeastOne)
+    {
+        for (int i = 0; i < 16; i++)
+        {
+            Serial.print(states[i] ? '1' : '0');
+        }
+        Serial.println();
+    }
+
     delay(5);
 }
